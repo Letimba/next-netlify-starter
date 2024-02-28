60ffe446ef5a77736680ddbbe5f679e1272f3b3d
Letimba <142944577+Letimba@users.noreply.github.com>
2024-02-28 01:40:02 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Notions #1 Template Site!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="NotionX" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <code>pages/index.js</code>
         </p>
       </main>
 
