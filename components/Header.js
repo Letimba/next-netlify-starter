94a57fd5eafa6086d5f9f58c797e4aac999817dd
Letimba <142944577+Letimba@users.noreply.github.com>
2024-02-28 01:56:18 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,43 @@
-export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
-}
+// components/Header.js
+import React from 'react';
+
+const Header = () => {
+  // Beispielhafte Hauptkategorien und Unterkategorien
+  const kategorien = [
+    { name: 'Home', subkategorien: [] },
+    {
+      name: 'Templates',
+      subkategorien: [
+        'Free Personal Notion Templates',
+        'Free Notion Templates for Business',
+        'Free Notion Templates for Students',
+        // Weitere Unterkategorien hier einfügen
+      ],
+    },
+    // Weitere Hauptkategorien hinzufügen
+  ];
+
+  return (
+    <header>
+      <div className="logo">NotionX</div>
+      <nav>
+        <ul className="hauptkategorien">
+          {kategorien.map((kategorie, index) => (
+            <li key={index}>
+              {kategorie.name}
+              {kategorie.subkategorien.length > 0 && (
+                <ul className="submenü">
+                  {kategorie.subkategorien.map((subkategorie, index) => (
+                    <li key={index}>{subkategorie}</li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
